b953fef0c3031fc4bf567f8eda60d638b6035f53
Greg Lieberman <kwarp11@gmail.com>
2015-07-20 23:51:48 -0700

    first commit
    blog example 1

---
+++ Assets/Scripts/BallInput.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallInput : MonoBehaviour 
+{
+	
+	public Rigidbody2D rigidBody2D;
+	public float acceleration = 10f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+		pushBall();
+	}
+
+
+	/// <summary>
+	/// Accelerate the ball based on arrow key input
+	/// </summary>
+	private void pushBall()
+	{
+		Vector2 force = Vector2.zero;
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			force.x -= acceleration;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			force.x += acceleration;
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			force.y += acceleration;
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			force.y -= acceleration;
+		}
+		
+		// apply force
+		rigidBody2D.AddForce (force);
+	}
+}
---
+++ Assets/Scripts/BallRotation.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallRotation : MonoBehaviour 
+{
+
+	public float radius = 0.5f;
+
+	private Vector3 lastPosition;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		lastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void FixedUpdate()
+	{
+		rotateBall();
+
+		lastPosition = transform.position;
+	}
+
+
+
+	/// <summary>
+	/// Rotate the 3d mesh with quaternions!
+	/// </summary>
+	private void rotateBall()
+	{
+		// calculate rotation based on the positional movement of the ball
+		// between the last frame and this one
+		Vector3 currentPosition = transform.position;
+		Vector3 currentBottom = new Vector3 (
+			currentPosition.x, 
+			currentPosition.y, 
+			currentPosition.z + radius); // the bottom of the ball this update
+		Vector3 lastBottom = new Vector3 (
+			lastPosition.x, 
+			lastPosition.y, 
+			lastPosition.z + radius); // the bottom of the ball last update
+
+		// direction Vectors that appriximate how much the ball rotated
+		// "good enough" for small movements between frames
+		Vector3 ballDown = currentBottom - currentPosition;
+		Vector3 ballRotated = lastBottom - currentPosition;
+
+		// store the rotation change in a Quaternion
+		Quaternion q = Quaternion.FromToRotation(ballDown, ballRotated); // that was easy!
+
+		// apply rotation to the transform
+		Quaternion newRotation = q * transform.rotation; // q must come first in multiplication order!!!
+		transform.rotation = newRotation;
+
+
+	}
+
+}
