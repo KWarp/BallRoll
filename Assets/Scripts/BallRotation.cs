d50edef95af6509c7373fad4e1d8d3ea1c99af7a
Greg Lieberman <kwarp11@gmail.com>
2015-07-29 01:39:12 -0700

    algorithm revisions

--- Assets/Scripts/BallRotation.cs
+++ Assets/Scripts/BallRotation.cs
@@ -28,49 +28,53 @@ public class BallRotation : MonoBehaviour
 	}
 	
 	/// <summary>
-	/// Rotate the 3d mesh with quaternions!
+	/// Rotate the 3D mesh with quaternions!
 	/// </summary>
 	private void rotateBall()
 	{
-		// calculate rotation based on the positional movement of the ball
-		// between the last frame and this one
-		Vector3 currentPosition = transform.position;
-		Vector3 currentBottom = new Vector3 (
-			currentPosition.x, 
-			currentPosition.y, 
-			currentPosition.z + radius); // the bottom of the ball this update
-		Vector3 lastBottom = new Vector3 (
-			lastPosition.x, 
-			lastPosition.y, 
-			lastPosition.z + radius); // the bottom of the ball last update
-
-		// direction Vectors
-		Vector3 ballDown = currentBottom - currentPosition;
-		Vector3 ballRotated = lastBottom - currentPosition;
-
-/*
-		// faster (but less accurate) rotation approximation
-		// store the rotation change in a Quaternion
-		Quaternion qSimple = Quaternion.FromToRotation(ballDown, ballRotated);
-		
-		// apply rotation to the transform
-		// qSimple must come first in multiplication order!!!
-		transform.rotation = qSimple * transform.rotation;
-		return;
-*/
-
-		// segment length that the ball rotated
-		Vector3 distance = currentBottom - lastBottom;
-		float segment = distance.magnitude;
-
-		if (segment == 0 || radius == 0) 
+		if (radius == 0) 
 		{
-			// nothing to do
+			// can't rotate ball
+			return; 
+		}
+
+		// distance the ball traveled between the last Update and this one
+		// we subtract the vectors in this order to use it as a direction vector later
+		Vector3 currentToLast = lastPosition - transform.position; 
+
+		// segment length that the ball rolled along its surface
+		float segment = currentToLast.magnitude;
+
+		if (segment == 0) 
+		{
+			// no distance travelled, nothing to do
 			return;
 		}
 
 		// to build a Quaternion to hold the desired rotation, 
 		// we need a Vector3 "axis" and an angle "theta"
+		// first, the axis:
+
+		// define the down direction vector for the ball
+		// the ball rolls in the x and y directions,
+		// and positive z points to the ground
+		// Important: 	this is DIFFERENT from Vector3.down because Vector3 assumes 
+		//				a 3D world space where negative y is down
+		Vector3 ballDown = new Vector3(0, 0, 1); 
+		
+		// use Cross Product to find the axis of rotation 
+		// https://www.mathsisfun.com/algebra/vectors-cross-product.html
+		Vector3 axis = Vector3.Cross(ballDown, currentToLast);
+
+		// Cross Product will fail if both vectors are parallel or perpendicular
+		if (axis == Vector3.zero) 
+		{
+			// this should never happen because currentToLast.z is always 0
+			// but who knows where this code will be copy-pasted to...
+			return;
+		}
+
+		// next, the angle theta:
 
 		// arc length formula
 		// s = r * theta
@@ -79,15 +83,12 @@ public class BallRotation : MonoBehaviour
 		float theta = segment / radius; // in radians
 		float thetaDegrees = theta * 180 / Mathf.PI;
 
-
-		// use Cross Product to find an axis of rotation 
-		Vector3 axis = Vector3.Cross(ballDown, ballRotated);
-
-		// the axis and angle is enough to make a rotation Quaternion!
+		// create the rotation Quaternion
 		Quaternion q = Quaternion.AngleAxis(thetaDegrees, axis);
 
 		// apply rotation to the transform
 		// q must come first in multiplication order!!!
+		// Quaternion multiplication is Non-Commutative!
 		transform.rotation = q * transform.rotation;
 	}
 
